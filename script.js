7ace096f2b0e910de7b62b224996ed6c2ace7c0f
rahmaptr <rahmaputriazzahra@gmail.com>
2022-08-06 20:38:48 +0700

    Add function

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
 const body = document.body;
 const container = document.createElement("div");
-const button = document.querySelector("#gridSize")
+const button = document.querySelector("#gridSize");
 container.setAttribute("id", "container");
 body.appendChild(container);
 
-for (let i = 0; i < 16; i++) {
-  for (let j = 0; j < 16; j++) {
-    const cell = document.createElement("div");
-    let cellName = "cell-" + i + "-" + j;
-    cell.classList.add("cell");
-    cell.setAttribute("id", cellName);
-    container.appendChild(cell);
-  }
-}
+mak
 
 for (let i = 0; i < 16; i++) {
   for (let j = 0; j < 16; j++) {
@@ -23,6 +15,31 @@ for (let i = 0; i < 16; i++) {
         cell.style.backgroundColor = 'red';
       })
   }
+}
+
+button.addEventListener("click", changeGridSize)
+
+function makeGrid(gridSize) {
+  for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
+      const cell = document.createElement("div");
+      let cellName = "cell-" + i + "-" + j;
+      cell.classList.add("cell");
+      cell.setAttribute("id", cellName);
+      container.appendChild(cell);
+    }
+  }
+}
+
+function changeGridSize() {
+  let gridSize = parseInt(window.prompt("Input custom grid size"));
+  
+  if (gridSize > 100) {
+    alert("Grid can't be bigger than 100");
+    gridSize = parseInt(window.prompt("Input custom grid size"));
+  }
+
+
 }
 
 
